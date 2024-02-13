44042173a79a20fe779ff068da0167d839b98be5
Apheru <pythonic.apheru@gmail.com>
2024-02-13 02:48:26 -0300

    day001

---
+++ day001/main.py
@@ -0,0 +1,17 @@
+# Day 001 Challenge 
+
+print("Hello, CodeSnakes!")
+print("#100DaysOfCode")
+print()
+print("Apheru")
+print("January 14th 2024")
+print()
+print("I am signing up for Replit's 100 days of Python challenge!")
+print()
+print("""
+I will make sure to spend some time every day coding along, for a minimum of 1 hour a day. I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from middle of a field!    
+""")
+print()
+print("I am feeling 🤩")
+print()
+print("You can follow my progress at replit.com/@pythonicapheru and @apherudev in instagram")
