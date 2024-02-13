c0d2d7112b6739a20db03f9250807595d636ae2b
Apheru <pythonic.apheru@gmail.com>
2024-02-13 04:51:40 -0300

    day006

---
+++ day006/challenge.py
@@ -0,0 +1,17 @@
+print("MEU SISTEMA DE LOGIN")
+print("++++++++++++++++++++")
+print()
+username = input("USERNAME >> ")
+password = input("PASSWORD >> ")
+print()
+
+if username == "Lucas" and password == "kukamaluka":
+    print("Salve Guerreiro, continua firme na caminhada!")
+elif username == "Karol" and password == "bunduda":
+    print("Oiie bundudinha! Te Amo de montão! <3")
+elif username == "Kevin" and password == "shadowzin":
+    print("Fala Kevinho, como você tá lindão? Papai te ama!")
+elif username == "Oliver" and password == "euhnenem":
+    print("Oii meu pequenino, você é muito especial! <3")
+else:
+    print("Você não possui nenhum cadastro no sistema!")
---
+++ day006/main.py
@@ -0,0 +1,16 @@
+print("SISTEMA DE LOGIN!")
+print()
+username = input("username >>")
+password = input("password >>")
+print()
+
+if username == "Lucas" and password =="chavoso":
+    print("Salve Lucão meu querido!")
+elif username == "Kevin" and password == "kevinho":
+    print("Fala Kevinho, suave?")
+elif username == "Karol" and password == "gatinhadopapai":
+    print("Oiie minha Gata! <3")
+elif username == "Oliver" and password == "euhnenem":
+    print("Oiie meu pequenino! <3")
+else:
+    print("SAI DAQUI SEU CACHORRO!")
