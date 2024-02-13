20f01c82dc80008aad13e703917e30297dfc96e0
Apheru <pythonic.apheru@gmail.com>
2024-02-13 03:35:29 -0300

    day005

---
+++ day005/challenge.py
@@ -0,0 +1,28 @@
+print("Veremos como você seria no mundo de One Piece!")
+print("--")
+print()
+
+
+question0 = input("Você seria um Marinheiro ou Pirata? ")
+print()
+if question0 == "Marinheiro":
+    print("Bem-vindo(a) a MARINHA, filho(a)!")
+else:
+    print("O mar te aguarda para que possa viver sua nova aventura seu cachorro desprezível do mar!")
+print()
+
+question1 = input("Seria usuário de Akuma no mi? ")
+print()
+if question1 == "Sim":
+    print("Magnifico jovem!")
+    print()
+    question1_1 = input("Qual tipo de Akuma no mim escolheria? Logia, Paramecia ou Zoan? ")
+    if question1_1 == "Logia":
+        print("Então você seria um forte usuário de fruto com capacidade de controlar ou criar um elemento!")
+    elif question1_1 == "Paramecia":
+        print("Então você seria um forte usuário de fruto com capacidade de modificar sua estrutura corporal ou manipular substâncias ou ambiente a sua volta!")
+    else:
+        print("Então você seria um forte usuário de fruto com capacidade de se transformar em uma criatura, com variações entre o formato híbrido e completo!")
+else:
+    print("É bom treinar esse HAKI! O mar não está para brincadeiras!")
+    
---
+++ day005/main.py
@@ -0,0 +1,6 @@
+my_name = input("Qual o seu nome, cumpadih? ")
+print()
+if my_name == "Apheru":
+    print(f"Eae, {my_name}! Suave? ")
+else:
+    print("Quem é tu na fila do pão menor? VAZA!")
