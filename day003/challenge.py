bb86fcaf35fab38a3fea68ebb5398eb4bf00f0b4
Apheru <pythonic.apheru@gmail.com>
2024-02-13 03:18:08 -0300

    day003

---
+++ day003/challenge.py
@@ -0,0 +1,11 @@
+# Day 003 Challenge - 16 Janeiro, 2024
+
+name_food = input("Nome de uma comida: ")
+name_plant = input("Nome de um tipo de planta: ")
+name_cooking = input("Nome de um método de cozimento: ")
+burned_food = input("Com que palavras descreve comida queimada? ")
+item = input("Nome de uma ferramenta: ")
+
+print()
+print("MENU")
+print(name_food, "de", name_cooking, "com", burned_food, name_plant, "em uma cama de", item)
---
+++ day003/main.py
@@ -0,0 +1,6 @@
+# Day 003 - 100 Days of Code: Concatenação 16 Janeiro, 2024
+
+your_name = input("Qual o seu nome? ")
+what_year = input("Que ano é este? ")
+print()
+print(your_name, "acho que é", what_year)
